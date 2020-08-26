5d1ab1f39b3c0ab86d181b37d069c70b77cc8013
kundan kumar <kd.kundan9572@gmail.com>
2020-08-26 18:12:37 +0530

    completed

--- src/controller/PersonalityViewController.java
+++ src/controller/PersonalityViewController.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import service.PersonalityCalculator;
+
 
 @WebServlet(urlPatterns= {"/personality"})
 public class PersonalityViewController extends HttpServlet {
@@ -33,8 +35,14 @@ public class PersonalityViewController extends HttpServlet {
 		String options=request.getParameter("selectedOptions");
 		
 		System.out.println(options);
-				
-		/* 
+			
+		
+		PersonalityCalculator  obj= new PersonalityCalculator ();
+		String message= obj.findYourBrainType(options);
+		
+		
+		
+		
 
 		request.setAttribute("message", message);
 		
@@ -53,7 +61,7 @@ public class PersonalityViewController extends HttpServlet {
 				rd.forward(request, response);
 			}
 		
-		}*/
+		}
 		}
 	}
 
--- src/service/PersonalityCalculator.java
+++ src/service/PersonalityCalculator.java
@@ -1,2 +1,31 @@
 package service;
 
+public class PersonalityCalculator{
+	public String findYourBrainType (String options) {
+		int answer[]=findAnswers (options);
+		int A= answer[1]+answer[2]+answer[3]+answer[5]+answer[8]+answer[10]+answer[11]+answer[12]+answer[14]+answer[18]+answer[20] ;
+		int B = answer[4] +answer[6]+answer[7] + answer[9] + answer[13] + answer[15] + answer[16] +answer[17]+ answer[19];
+		int X = 66 -( A + B);
+		if(X ==20 && X <=55) {
+			return "Left-brained";
+		}
+		if(X >55 && X <=65) {
+			return "No clear preference";
+		
+		}
+		else 
+			return "Right-brained";
+		
+
+	}
+	public int[] findAnswers(String options) {
+		
+		String[] optionsTemp= options.split(",");
+		int answer[]= new int[20];
+		for(int i=0;i<=optionsTemp.length;i++) {
+			answer[i]=Integer.parseInt(optionsTemp[i]);
+		}
+		return answer;
+	}
+}
+
